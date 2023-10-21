a84aeb7bdb2c1d0ed33426f89ebf4ca549cfadcd
elshohan <josealwachu9@gmail.com>
2023-10-20 23:05:31 -0500

    Task Advance

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Task from "./components/list/Task";
 import Content from "./components/shared/Content";
 import Footer from "./components/shared/Footer";
 import Header from "./components/shared/Header";
@@ -6,7 +7,9 @@ export default function App() {
   return (
     <div>
       <Header></Header>
-      <Content></Content>
+      <Content>
+        <Task></Task>
+      </Content>
       <Footer></Footer>
     </div>
   )
---
+++ src/components/list/Task.jsx
@@ -0,0 +1,22 @@
+export default function Task() {
+  return(
+    <div>
+      <div>
+        <div>Icono</div>
+        <div>
+          <p>1<sub>/ semana</sub></p>
+          <p>Detalles</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <p>4 de 5</p>
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <button>Completado</button>
+      </div>
+    </div>
+  )
+}
